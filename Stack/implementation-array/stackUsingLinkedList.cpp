424a9256f09c9cc17c86c9d1b9183cebbe3dc892
gurjot <gurjotsaini53@gmail.com>
2023-03-15 13:30:01 +0530

    adding stack implementation using linked list

---
+++ Stack/implementation-array/stackUsingLinkedList.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+   public:
+   int data;
+   Node *next;
+
+   Node(int value)
+   {
+    this->data=value;
+    this->next= NULL;
+   }
+};
+class Stack{
+
+
+};
+
+
+
+int main(){
+
+
+}
